59d166b1af33388af1404ffb67eeb668d586e5a6
nargok <nargok@gmail.com>
2020-04-22 08:51:22 +0900

    test insertTodo

--- lib/data/todo_db.dart
+++ lib/data/todo_db.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter/cupertino.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'data/todo.dart';
 import 'data/todo_db.dart';
 
-void main() => runApp(Myapp());
+void main() => runApp(MyApp());
 
-class Myapp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,6 +26,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State {
   @override
   Widget build(BuildContext context) {
+    _testData();
     return Container();
+  }
+
+  Future _testData() async {
+    TodoDb db = TodoDb();
+    await db.database;
+    List<Todo> todos = await db.getTodos();
+    await db.deleteAll();
+    todos = await db.getTodos();
+
+    await db.insertTodo(Todo('Call Donald', 'And tell him about Daisy', '02/02/2020', 1));
+    await db.insertTodo(Todo('By sugar', '1 Kg, brown', '02/02/2020', 2));
+    await db.insertTodo(Todo('Go Running', '@12.00, with neigh-bours', '02/02/2020', 3));
+    todos = await db.getTodos();
+
+    debugPrint('First insert');
+    todos.forEach((Todo todo){
+      debugPrint(todo.name);
+    });
+
   }
 }
